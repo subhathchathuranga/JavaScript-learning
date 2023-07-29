1dcc6eb9a25714cf3bff5a1a1303d372be78e113
Subhath Chathuranga <csubhath@gmail.com>
2023-07-29 19:42:24 +0530

    new learnings

--- javas.js
+++ javas.js
@@ -1,7 +1,29 @@
+//print hello world
 console.log("Hello World");
 
+
+
+//variables
 let a;
-let b="subhath";
+let name="subhath";
 a= 5;
+
+//window.alert("Hi "+ name);
 console.log("number: ",a);
-console.log("name",b);
+console.log("name", name);
+
+
+//inner html 
+let fname="subhath";
+document.getElementById("myPara").innerHTML = "Hi "+fname;
+
+//onclick
+let username
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+}
+
+//conversion
+let age;
+age=Number(age);
