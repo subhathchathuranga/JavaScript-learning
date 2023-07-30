99eabe75987474c5a8981214a9359d41666c857e
Subhath Chathuranga <csubhath@gmail.com>
2023-07-30 09:30:10 +0530

    check box

--- javas.js
+++ javas.js
@@ -14,16 +14,36 @@ console.log("name", name);
 
 
 //inner html 
-let fname="subhath";
-document.getElementById("myPara").innerHTML = "Hi "+fname;
+//let fname="subhath";
+//document.getElementById("myPara").innerHTML = "Hi "+fname;
 
 //onclick
-let username
+/**let username
 document.getElementById("myButton").onclick = function(){
     username = document.getElementById("myText").value;
     console.log(username);
-}
+}**/
 
 //conversion
 let age;
-age=Number(age);
+//age=Number(age);
+
+
+//if condition
+/**age = 21;
+
+if(age >=18){
+    console.log("You are an adult");
+}
+else{
+    console.log("you are not an adult");
+}**/
+document.getElementById("myButton").onclick=function(){
+const checkBox=document.getElementById("myCheckBox");
+if(checkBox.checked){
+    window.alert("You are Subscribed!");
+}
+else{
+    window.alert("You are not Subscribed!");
+}
+}
